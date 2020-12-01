a966532de065d1e9650653eb0f98fd5111b18172
Likibi <likibikangben@gmail.com>
2020-12-01 12:14:16 +0000

    Modified products app and models

--- products/models.py
+++ products/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
-    numberOfTickets = models.DecimalField(max_digits=6, decimal_places=0)
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
